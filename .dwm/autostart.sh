58f355c5eed37b72ff07a5183408086ffc413480
Autocratic Binary <autocraticbinary@gmail.com>
2024-10-18 07:25:23 +0530

    xautolock killer

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,6 +1,7 @@
 picom -f &
 dwmblocks &
-xautolock -time 5 -locker slock &
+#xautolock -time 5 -locker slock &
+xautolock -detectsleep -time 5 -locker slock -killtime 20 -killer "loginctl suspend" &
 wmname LG3D
 feh --bg-scale ~/Pictures/wallpaper/arch1.png
 sxhkd -c ~/.config/sxhkd/sxhkdrc
