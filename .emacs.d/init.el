65e30b14eb87dd405da1e86e2543c08f5c6cc133
0xfarco <0xfarco@gmail.com>
2025-01-20 07:34:36 +0530

    new emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,32 +1,51 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-;;(setq custom-file "C://Users//0xfarco//.emacs.d//.emacs.custom.el")
 (setq custom-file "~/.emacs.d/.emacs.custom.el")
+;;(setq custom-file "C://Users//0xfarco//.emacs.d//.emacs.custom.el")
+
 (add-to-list 'load-path "~/.emacs.d/lang/")
-
-(load "~/.emacs.d/rc/rc.el")
-
-(load "~/.emacs.d/rc/misc-rc.el")
-(load "~/.emacs.d/rc/org-mode-rc.el")
-(load "~/.emacs.d/rc/autocommit-rc.el")
-
-(add-to-list 'default-frame-alist `(font . "JetBrainsMono Nerd Font-12"))
 
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
-(global-display-line-numbers-mode 1)
 (column-number-mode 1)
 (show-paren-mode 1)
+(global-display-line-numbers-mode 1)
+(electric-pair-mode 1)
+(whitespace-mode 1)
 
-(rc/require-theme 'gruber-darker)
-;; (rc/require-theme 'zenburn)
-;; (load-theme 'adwaita t)
+(setq display-line-numbers-type 'relative)
+
+(setq-default inhibit-splash-screen t
+			make-backup-files nil
+			tab-width 4
+			indent-tabs-mode nil
+			compilation-scroll-output t)
+
+(add-to-list 'default-frame-alist `(font . "JetBrainsMono Nerd Font-12"))
+
+(add-to-list 'custom-theme-load-path "/home/0xfarco/.emacs.d/themes/")
+(load-theme 'gruber-darker t)
+
+(use-package cl-libify
+	:ensure t)
+
+(use-package which-key
+	:ensure t
+	:config
+	(which-key-mode))
 
 ;;; ido
-(rc/require 'smex 'ido-completing-read+)
+(use-package smex
+	:ensure t)
 
-(require 'ido-completing-read+)
+(use-package ido-completing-read+
+	:ensure t)
 
 (ido-mode 1)
 (ido-everywhere 1)
@@ -35,7 +54,23 @@
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+(global-set-key (kbd "M-x") 'smex)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("s-p" . projectile-command-map)
+              ("C-c p" . projectile-command-map)))
+
 ;;; c-mode
+;; (setq-default c-basic-offset 4
+;;              c-default-style "linux")
+
 (setq-default c-basic-offset 4
               c-default-style '((java-mode . "java")
                                 (awk-mode . "awk")
@@ -44,41 +79,6 @@
 (add-hook 'c-mode-hook (lambda ()
                          (interactive)
                          (c-toggle-comment-style -1)))
-
-;;; Paredit
-(rc/require 'paredit)
-
-(defun rc/turn-on-paredit ()
-  (interactive)
-  (paredit-mode 1))
-
-(add-hook 'emacs-lisp-mode-hook  'rc/turn-on-paredit)
-(add-hook 'clojure-mode-hook     'rc/turn-on-paredit)
-(add-hook 'lisp-mode-hook        'rc/turn-on-paredit)
-(add-hook 'common-lisp-mode-hook 'rc/turn-on-paredit)
-(add-hook 'scheme-mode-hook      'rc/turn-on-paredit)
-(add-hook 'racket-mode-hook      'rc/turn-on-paredit)
-
-;;; Emacs lisp
-(add-hook 'emacs-lisp-mode-hook
-          '(lambda ()
-             (local-set-key (kbd "C-c C-j")
-                            (quote eval-print-last-sexp))))
-(add-to-list 'auto-mode-alist '("Cask" . emacs-lisp-mode))
-
-;;; uxntal-mode
-
-(rc/require 'uxntal-mode)
-
-;;; Haskell mode
-(rc/require 'haskell-mode)
-
-(setq haskell-process-type 'cabal-new-repl)
-(setq haskell-process-log t)
-
-(add-hook 'haskell-mode-hook 'haskell-indent-mode)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'haskell-mode-hook 'haskell-doc-mode)
 
 (require 'basm-mode)
 
@@ -96,58 +96,14 @@
 
 (require 'c3-mode)
 
-;;; Whitespace mode
-(defun rc/set-up-whitespace-handling ()
-  (interactive)
-  (whitespace-mode 1)
-  (add-to-list 'write-file-functions 'delete-trailing-whitespace))
+;; Define the whitespace style.
+(setq-default whitespace-style
+              '(face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark))
 
-(add-hook 'tuareg-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'c++-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'c-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'simpc-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'emacs-lisp-mode 'rc/set-up-whitespace-handling)
-(add-hook 'java-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'lua-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'rust-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'scala-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'markdown-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'haskell-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'python-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'erlang-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'asm-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'fasm-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'go-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'nim-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'yaml-mode-hook 'rc/set-up-whitespace-handling)
-(add-hook 'porth-mode-hook 'rc/set-up-whitespace-handling)
+;;; Dired
+;;(use-package dired
+;;	:ensure t)
 
-;;; display-line-numbers-mode
-;;(when (version<= "26.0.50" emacs-version)
-;;  (global-display-line-numbers-mode))
-
-;;; magit
-;; magit requres this lib, but it is not installed automatically on
-;; Windows.
-(rc/require 'cl-lib)
-(rc/require 'magit)
-
-(setq magit-auto-revert-mode nil)
-
-(global-set-key (kbd "C-c m s") 'magit-status)
-(global-set-key (kbd "C-c m l") 'magit-log)
-
-;;; multiple cursors
-;;(rc/require 'multiple-cursors)
-
-;;(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-;;(global-set-key (kbd "C->")         'mc/mark-next-like-this)
-;;(global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
-;;(global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
-;;(global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
-;;(global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
-
-;;; dired
 (require 'dired-x)
 (setq dired-omit-files
       (concat dired-omit-files "\\|^\\..+$"))
@@ -155,139 +111,77 @@
 (setq dired-listing-switches "-alh")
 (setq dired-mouse-drag-files t)
 
-;;; helm
-(rc/require 'helm 'helm-git-grep 'helm-ls-git)
-
-(setq helm-ff-transformer-show-only-basename nil)
-
-(global-set-key (kbd "C-c h t") 'helm-cmd-t)
-(global-set-key (kbd "C-c h g g") 'helm-git-grep)
-(global-set-key (kbd "C-c h g l") 'helm-ls-git-ls)
-(global-set-key (kbd "C-c h f") 'helm-find)
-(global-set-key (kbd "C-c h a") 'helm-org-agenda-files-headings)
-(global-set-key (kbd "C-c h r") 'helm-recentf)
-
 ;;; yasnippet
-(rc/require 'yasnippet)
-
-(require 'yasnippet)
+(use-package yasnippet
+	:ensure t)
 
 (setq yas/triggers-in-field nil)
 (setq yas-snippet-dirs '("~/.emacs.d/snippets/"))
 
 (yas-global-mode 1)
 
-;;; word-wrap
-(defun rc/enable-word-wrap ()
-  (interactive)
-  (toggle-word-wrap 1))
-
-(add-hook 'markdown-mode-hook 'rc/enable-word-wrap)
-
-;;; nxml
-(add-to-list 'auto-mode-alist '("\\.html\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.xsd\\'" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.ant\\'" . nxml-mode))
-
-;;; tramp
-;;; http://stackoverflow.com/questions/13794433/how-to-disable-autosave-for-tramp-buffers-in-emacs
-(setq tramp-auto-save-directory "/tmp")
-
 ;;; powershell
-(rc/require 'powershell)
+(use-package powershell
+	:ensure t)
 (add-to-list 'auto-mode-alist '("\\.ps1\\'" . powershell-mode))
 (add-to-list 'auto-mode-alist '("\\.psm1\\'" . powershell-mode))
 
-;;; eldoc mode
-;;(defun rc/turn-on-eldoc-mode ()
-;;  (interactive)
-;;  (eldoc-mode 1))
-
-;;(add-hook 'emacs-lisp-mode-hook 'rc/turn-on-eldoc-mode)
-
-;;; Company
-(rc/require 'company)
-(require 'company)
-
-(global-company-mode)
-
-(add-hook 'tuareg-mode-hook
-          (lambda ()
-            (interactive)
-            (company-mode 0)))
-
-;;; Typescript
-(rc/require 'typescript-mode)
-(add-to-list 'auto-mode-alist '("\\.mts\\'" . typescript-mode))
-
-;;; Tide
-(rc/require 'tide)
-
-(defun rc/turn-on-tide-and-flycheck ()  ;Flycheck is a dependency of tide
-  (interactive)
-  (tide-setup)
-  (flycheck-mode 1))
-
-(add-hook 'typescript-mode-hook 'rc/turn-on-tide-and-flycheck)
-
-;;; Proof general
-;;(rc/require 'proof-general)
-;;(add-hook 'coq-mode-hook
-;;          '(lambda ()
-;;             (local-set-key (kbd "C-c C-q C-n")
-;;                            (quote proof-assert-until-point-interactive))))
-
-;;; LaTeX mode
-(add-hook 'tex-mode-hook
-          (lambda ()
-            (interactive)
-            (add-to-list 'tex-verbatim-environments "code")))
-
-(setq font-latex-fontify-sectioning 'color)
+;;; company
+(use-package company
+	:ensure t)
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;;; Move Text
-(rc/require 'move-text)
+(use-package move-text
+	:ensure t)
 (global-set-key (kbd "M-p") 'move-text-up)
 (global-set-key (kbd "M-n") 'move-text-down)
 
-;;; Ebisp
-;;(add-to-list 'auto-mode-alist '("\\.ebi\\'" . lisp-mode))
+(use-package go-mode
+	:ensure t)
+;; (add-to-list 'load-path "/place/where/you/put/it/")
+;; (autoload 'go-mode "go-mode" nil t)
+;; (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
 
-;;; Packages that don't require configuration
-(rc/require
- 'scala-mode
- 'd-mode
- 'yaml-mode
- 'glsl-mode
- 'tuareg
- 'lua-mode
- 'less-css-mode
- 'graphviz-dot-mode
- 'clojure-mode
- 'cmake-mode
- 'rust-mode
- 'csharp-mode
- 'nim-mode
- 'jinja2-mode
- 'markdown-mode
- 'purescript-mode
- 'nix-mode
- 'dockerfile-mode
- 'toml-mode
- 'nginx-mode
- 'kotlin-mode
- 'go-mode
- 'php-mode
- 'racket-mode
- 'qml-mode
- 'ag
- 'elpy
- 'typescript-mode
- 'rfc-mode
- 'sml-mode
- )
+(use-package rust-mode
+	:ensure t)
 
-(load "~/.emacs.shadow/shadow-rc.el" t)
+(use-package php-mode
+ 	:ensure t)
+
+(use-package markdown-mode
+	:ensure t)
+
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
+
+(use-package nim-mode
+	:ensure t)
+
+(use-package typescript-mode
+	:ensure t)
+
+(use-package lua-mode
+	:ensure t)
+
+;; (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+;; (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+;; (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+;; (use-package js2-mode
+;; 	:ensure t)
+;; 
+;; (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+;; (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+;; (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+
+(use-package dockerfile-mode
+	:ensure t)
+
+(setq dockerfile-mode-command "docker")
 
 (defun astyle-buffer (&optional justify)
   (interactive)
@@ -305,14 +199,12 @@
             (interactive)
             (setq-local fill-paragraph-function 'astyle-buffer)))
 
-(require 'compile)
-
-;; pascalik.pas(24,44) Error: Can't evaluate constant expression
-
 compilation-error-regexp-alist-alist
 
 (add-to-list 'compilation-error-regexp-alist
-             '("\\([a-zA-Z0-9\\.]+\\)(\\([0-9]+\\)\\(,\\([0-9]+\\)\\)?) \\(Warning:\\)?"
-               1 2 (4) (5)))
+            '("\\([a-zA-Z0-9\\.]+\\)(\\([0-9]+\\)\\(,\\([0-9]+\\)\\)?) \\(Warning:\\)?"
+              1 2 (4) (5)))
+
+(setq warning-minimum-level :error)
 
 (load-file custom-file)
