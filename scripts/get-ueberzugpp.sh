4e3eeb770ff9d50f122ba01479fc1c2ee6df17cb
0xfarco <0xfarco@gmail.com>
2025-05-16 10:59:35 +0530

    change get-ueberzugpp.sh

--- scripts/get-ueberzugpp.sh
+++ scripts/get-ueberzugpp.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+set -e
 
-sudo pacman -S cmake libvips libsixel chafa openssl onetbb opencv xcb-util-image extra-cmake-modules fmt spdlog
+# Ensure bin directory exists
+mkdir -p "$HOME/.local/bin"
 
-git clone https://github.com/0xfarco/ueberzugpp $HOME/Projects/ueberzugpp
-cd /home/farco/Projects/ueberzugpp
-mkdir build && cd build
+# Install dependencies
+sudo pacman -S --noconfirm cmake libvips libsixel chafa openssl onetbb xcb-util-image extra-cmake-modules fmt spdlog range-v3 cli11 nlohmann-json
+
+# Clone and build ueberzugpp
+git clone https://github.com/0xfarco/ueberzugpp "$HOME/.local/src/ueberzugpp"
+cd "$HOME/.local/src/ueberzugpp"
+mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
+
+# Copy the binary
+cp ueberzugpp "$HOME/.local/bin/"
+
