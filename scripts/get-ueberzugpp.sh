82c0bbe9f4cda7c281bf404109424d20c38987a9
0xfarco <0xfarco@gmail.com>
2025-05-19 03:07:17 +0530

    Add Void Linux support

--- scripts/get-ueberzugpp.sh
+++ scripts/get-ueberzugpp.sh
@@ -2,10 +2,25 @@
 set -e
 
 # Ensure bin directory exists
-mkdir -p "$HOME/.local/bin"
+#mkdir -p "$HOME/.local/bin"
+#mkdir -p "$HOME/.local/src"
+
+# Detect distribution
+if [ -f /etc/arch-release ]; then
+    DISTRO="arch"
+elif [ -f /etc/void-release ]; then
+    DISTRO="void"
+else
+    echo "Unsupported distribution"
+    exit 1
+fi
 
 # Install dependencies
-sudo pacman -S --noconfirm cmake libvips libsixel chafa openssl onetbb xcb-util-image extra-cmake-modules fmt spdlog range-v3 cli11 nlohmann-json
+if [ "$DISTRO" = "arch" ]; then
+    sudo pacman -S --noconfirm cmake libvips libsixel chafa openssl onetbb xcb-util-image extra-cmake-modules fmt spdlog range-v3 cli11 nlohmann-json
+elif [ "$DISTRO" = "void" ]; then
+    sudo xbps-install -Sy cmake libvips libsixel chafa openssl tbb xcb-util-image fmt spdlog range-v3 extra-cmake-modules
+fi
 
 # Clone and build ueberzugpp
 git clone https://github.com/0xfarco/ueberzugpp "$HOME/.local/src/ueberzugpp"
